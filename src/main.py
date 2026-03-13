a3804ef0d51533cd7a1f1d48efddfb5465a1b7c0
MatteoStromieri <matteo.stromieri@outlook.it>
2026-03-13 14:05:05 +0100

    removed comments from src/main

--- src/main.py
+++ src/main.py
@@ -2,15 +2,11 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 from src.pipelines import master_pipeline
 
-# The @hydra.main decorator takes over the role of argparse.
 # - config_path points to the directory containing your configs (e.g., 'conf/')
 # - config_name is the default yaml file to load (without the .yaml extension)
 @hydra.main(version_base=None, config_path="../config")
 def main(cfg: DictConfig):
     
-    # Optional: If your run_pipeline function requires a standard Python dictionary
-    # rather than a Hydra DictConfig object, you can convert it like this.
-    # The `resolve=True` argument ensures all interpolations (like ${task.name}) are calculated.
     config_dict = OmegaConf.to_container(cfg, resolve=True)
 
     print("=== FULL COMPOSED CONFIG ===")
@@ -21,5 +17,4 @@ def main(cfg: DictConfig):
     master_pipeline.run_pipeline(config_dict)
 
 if __name__ == "__main__":
-    # You just call main() directly. Hydra will automatically inject 'cfg'.
     main()
